6c997b92e97019cc6a3814313cd5deb34a1f7ab2
Pratap Padhy <pratappadhy2005@gmail.com>
2026-02-08 10:43:52 +1100

    -Added testing flux using stepverifier

--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,27 @@
 package com.learnreactiveprogramming.service;
 
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FluxAndMonoGeneratorServiceTest {
+
+    FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+
+    @Test
+    void namesFlux() {
+        //given
+
+        //when
+        var namesFlux = fluxAndMonoGeneratorService.namesFlux();
+
+        //then
+        StepVerifier.create(namesFlux)
+                //.expectNext("alex", "ben", "chloe")
+                .expectNext("alex")
+                .expectNextCount(2)
+                .verifyComplete();
+    }
 
 }
