411a59c87e449d9c639c965b9961f2f84a6d218a
Pratap Padhy <pratappadhy2005@gmail.com>
2026-02-07 21:38:57 +1100

    -Added initial Spring reactive skeleton

---
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -0,0 +1,18 @@
+package com.learnreactiveprogramming.service;
+
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+public class FluxAndMonoGeneratorService {
+
+    public Flux<String> namesFlux() {
+        return Flux.fromIterable(List.of("alex", "ben", "chloe"));
+    }
+
+    public static void main(String[] args) {
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+        fluxAndMonoGeneratorService.namesFlux()
+                .subscribe(name -> System.out.println("Name is : " + name));
+    }
+}
